ec5c4b5429101b8339c4c34efbfdc6e29ec47e23
Facundo Muñoz <facundo.munoz@cirad.fr>
2018-06-15 12:02:51 +0200

    remove a few testing warnigns

--- tests/testthat/test-align_layers.R
+++ tests/testthat/test-align_layers.R
@@ -12,8 +12,11 @@ test_that("handle rasters without projection information", {
 
 test_that("harmonise rasters with different projections", {
   
-  adp <- projectRaster(animald, crs = CRS("+init=epsg:26978"))
-  aligned <- align_layers(list(animald, adp, animald))
+  ## Albers Equal Area Africa 1 CRS
+  ## http://spatialreference.org/ref/sr-org/8476/
+  proj.srorg8476 <- "+proj=aea +lat_1=36.5 +lat_2=29.071428571429 +lat_0=32.7857142857145 +lon_0=-14.111111111111 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs"
+  adp <- suppressWarnings(projectRaster(animald, crs = CRS(proj.srorg8476)))
+  aligned <- suppressWarnings(align_layers(list(animald, adp, animald)))
   expect_identical(
     unique(vapply(aligned, proj4string, character(1))),
     proj4string(animald)

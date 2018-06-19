19baf1341d55996812ce199bd9f5ba75484d0204
Facundo Muñoz <facundo.munoz@cirad.fr>
2018-06-19 14:37:15 +0200

    pkg_deps(): list of package dependencies

---
+++ R/pkg_deps.R
@@ -0,0 +1,12 @@
+#' mapMCDA package dependencies
+#' 
+#' The list of packages has been recovered after the installation of the package
+#' on a clean Windows 7 virtual machine.
+#'
+#' @return character vector of package names
+#'
+pkg_deps <- function() {
+  deps <- system.file(file.path("doc", "mapMCDA_deps"), package = "mapMCDA")
+  deps <- readLines(deps)
+  return(deps)
+}

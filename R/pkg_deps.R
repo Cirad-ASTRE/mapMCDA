0c8339bbf80e0fcfd6d488b894b9b0fb939918cb
Facundo Muñoz <facundo.munoz@cirad.fr>
2018-06-20 16:54:35 +0200

    update package dependencies

--- R/pkg_deps.R
+++ R/pkg_deps.R
@@ -1,7 +1,8 @@
 #' mapMCDA package dependencies
 #' 
 #' The list of packages has been recovered after the installation of the package
-#' on a clean Windows 7 virtual machine.
+#' on a clean Windows 7 virtual machine. It also gathers some further packages
+#' installed in Appveyor.
 #'
 #' @return character vector of package names
 #'

f19e0e61eec9493f7180ef38dcc92e5a2de69a99
Facundo Muñoz <facundo.munoz@cirad.fr>
2018-06-21 11:45:42 +0200

    graphical representation of dependencies

--- R/pkg_deps.R
+++ R/pkg_deps.R
@@ -51,3 +51,55 @@ download_deps <- function(dir, type = 'all', pkgs = pkg_deps()) {
 
 # download_deps("../prodel/Rpkgs", type = "all", pkgs = pkg_deps())
 
+
+# Build graph of dependencies
+# plot(breedR.deps_graph())
+# fname <- '../../doc/breedR-dev/dependencies.gml'
+# write_graph(breedR.deps_graph(),
+#             file = fname,
+#             format = 'gml')
+# # in the resulting file, change 'name' by 'label'
+# # in order to open it with yEd
+# system(paste("sed -i 's/name/label/g'", fname))
+deps_graph <- function(which = c('Depends', 'Imports')) {
+  if (!require(igraph))
+    stop('This requires installing igraph.')
+  
+  inst <- installed.packages()
+  base <- unname(inst[inst[, "Priority"] %in% c("base", "recommended"), 
+                      "Package"])
+  
+  direct_deps <- dev_package_deps(dependencies = NA)$package
+  
+  deps <- setdiff(direct_deps, base)
+  
+  # available = available.packages()
+  available = inst
+  
+  dep_list <- list(mapMCDA = deps)
+  new_deps <- deps
+  count = 0
+  while (length(new_deps)>0 && count < 10) {
+    all_deps <- tools::package_dependencies(new_deps, db = available)
+    deps_clean <- sapply(all_deps, setdiff, base)
+    deps_clean <- deps_clean[sapply(deps_clean, length) > 0]
+    dep_list <- c(dep_list, deps_clean)
+    new_deps <- unique(unname(unlist(deps_clean)))
+    count = count + 1
+  }
+  
+  dep_full <- 
+    unname(
+      unlist(
+        sapply(names(dep_list),
+               function(x) as.vector(sapply(dep_list[[x]], 
+                                            function(y) c(x, y)))
+        )
+      )
+    )
+  
+  return(make_graph(dep_full))
+}
+
+# require(visNetwork)
+# visIgraph(deps_graph())

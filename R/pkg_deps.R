23dd42e9f63d81e2ddbe7ace6bdfcdaec8f56f1f
Facundo Muñoz <facundo.munoz@cirad.fr>
2018-06-25 16:31:20 +0200

    devel: download package dependencies for Rversions

--- R/pkg_deps.R
+++ R/pkg_deps.R
@@ -34,15 +34,41 @@ download_deps <- function(dir, type = 'all', pkgs = pkg_deps()) {
   type <- match.arg(type, c('all', typelst), several.ok = TRUE)
   if ('all' %in% type) type <- typelst
   
-  for (t in type) {
-    dest <- file.path(dir, t)
+  Rver = gsub(
+    "\\.\\d$", "",
+    c(release = rversions::r_release()$version,
+      oldrel = rversions::r_oldrel()$version)
+  )
+
+  contrib_version <- function(type, ver) {
+    gsub(
+      "\\d\\.\\d$", ver,
+      contrib.url(getOption("repos")["CRAN"], type)
+    )
+  }
+  
+  contribs <- expand.grid(type = type, ver = Rver, stringsAsFactors = FALSE)
+  contribs$conturl <- purrr::map2(contribs$type, contribs$ver, contrib_version)
+  contribs <- contribs[!duplicated(contribs[, c("type", "conturl")]),]
+  
+  ans <- structure(vector("list", nrow(contribs)), names = contribs$type)
+  for (i in seq_len(nrow(contribs))) {
+    dest <- file.path(dir, paste(contribs[i, c("type", "ver")], collapse = "-"))
     dir.create(dest)
-    utils::download.packages(pkgs, destdir = dest, type = t)
+    ans[[i]] <- utils::download.packages(
+      pkgs,
+      destdir = dest,
+      repos = getOption("repos")["CRAN"],
+      contriburl = contribs[i, "conturl"],
+      type = contribs[i, "type"]
+    )
   }
   
   message('Done. ',
           'Don\'t forget to include also the ',
           'source and compiled packages for mapMCDA.')
+  
+  return(do.call("rbind", ans))
 }
 
 # download_deps("../prodel/Rpkgs", type = "all", pkgs = pkg_deps())
@@ -94,3 +120,26 @@ deps_graph <- function(which = c('Depends', 'Imports')) {
 
 # require(visNetwork)
 # visIgraph(deps_graph())
+
+
+## Usage:
+## repo <- file.path('file://', path.expand('~/t4f/pkgrepo'))
+## available.packages(contrib.url(repo, 'source'))
+## install.packages(pkgs[, 'Package'], repos = repo)
+update_drat <- function(
+  repodir  = '~/Work/Proyectos/prodel/pkgrepo',
+  win.binary = NULL
+) {
+  file <- devtools::build(vignettes = TRUE)
+  drat::insertPackage(file, repodir)
+  
+  srcdir <- tempdir()
+  downloaded_pkgs <- download_deps(dir = srcdir, type = 'all')
+  for (fn in downloaded_pkgs[, 2]) {
+    drat::insertPackage(fn, repodir, action = "prune")
+  }
+  
+  if (!is.null(win.binary)) {
+    drat::insertPackage(win.binary, repodir)
+  }
+}

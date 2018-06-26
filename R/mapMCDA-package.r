670a6cb6d372707ad53d58a81e1be2af2dc6e9f2
Facundo Muñoz <facundo.munoz@cirad.fr>
2018-06-26 18:33:04 +0200

    package help

---
+++ R/mapMCDA-package.r
@@ -0,0 +1,11 @@
+#' mapMCDA
+#' 
+#' Produce an epidemiological risk map by weighting multiple risk factors
+#'
+#' @name mapMCDA
+#' @docType package
+NULL
+
+.onLoad <- function(libname, pkgname) {
+  mapMCDA_app()
+}
